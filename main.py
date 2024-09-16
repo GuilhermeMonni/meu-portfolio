386a44948f843f43e56b435cf17442ff670cb4c8
Guilherme Monni <gmonni20@gmail.com>
2024-09-16 19:15:14 -0300

    rep

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+import pyautogui
+
+time.sleep(3)
+
+print(pyautogui.position())
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
-function cursor(){ /* onmousemove do container */
+// Quantidade de estrelas
+const numStars = 300;
+
+// Função para gerar as estrelas
+function generateStars() {
+  const stars = document.querySelector('.stars');
+  let sombra = '';
+
+  for (let i = 0; i < numStars; i++) {
+    // Gera posições aleatórias para cada estrela
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
     
+    //define a posicao do box-shadow
+    sombra += `${x}px ${y}px #FFF${i < numStars - 1 ? ',' : ''}`;
+  }
+
+  // Aplica o box-shadow gerado ao elemento .stars
+  stars.style.boxShadow = sombra;
 }
+
+// Gera as estrelas ao carregar a página
+window.onload = generateStars;
