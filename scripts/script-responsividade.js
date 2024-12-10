19e3db9295f357412e626ae7ac2101d5cfaf7f95
Guilherme Monni <gmonni20@gmail.com>
2024-12-10 17:41:52 -0300

    ajuste planos

---
+++ scripts/script-responsividade.js
@@ -0,0 +1,8 @@
+const smallWindow = window.matchMedia('(max-width: 1500px)')
+
+// Condição de tela menor
+if (smallWindow.matches) {
+  // Cards
+  const cardsDiv = document.querySelector('.cards')
+  // Animação scroll
+}
