ef2e250cee4f61773f9d826a75acc4ce04fc6c46
Guilherme Monni <gmonni20@gmail.com>
2024-12-10 20:58:46 -0300

    meu portfolio

--- scripts/script-responsividade.js
+++ scripts/script-responsividade.js
@@ -1,18 +1,24 @@
 const smallWindow = window.matchMedia('(max-width: 1500px)')
 
 // Animação scroll
-window.addEventListener('scroll', () => {
-  const cards = document.querySelectorAll('.scroll')
-  const windowWid = window.innerWidth * 1.8
+if(smallWindow.matches){
+  window.addEventListener('scroll', () => {
+    const cards = document.querySelectorAll('.scroll')
+    const windowWid = window.innerWidth * 1.8
 
-  cards.forEach((item) => {
-    const cardTop = item.getBoundingClientRect().top - windowWid
+    cards.forEach((item) => {
+      const cardTop = item.getBoundingClientRect().top - windowWid
 
-    if(cardTop < 0){
-      item.classList.add('scrollOn')
-      item.classList.remove('scroll')
-    } else{
-      item.classList.remove('scrollOn')
-    }
+      if(cardTop < 0){
+        item.classList.add('scrollOn')
+        item.classList.remove('scroll')
+      } else{
+        item.classList.remove('scrollOn')
+      }
+    })
   })
-})
+}else{
+  const cards = document.querySelectorAll('.scroll')
+
+  cards.classList.remove('scroll')
+}
