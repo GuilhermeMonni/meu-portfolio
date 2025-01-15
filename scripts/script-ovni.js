a9f1f2385cb6647bcdb7e70c8a4ea3d003240437
Guilherme Monni <gmonni20@gmail.com>
2025-01-15 18:52:29 -0300

    ovni

--- scripts/script-ovni.js
+++ scripts/script-ovni.js
@@ -2,10 +2,7 @@
 const ovni = document.querySelector('.ovni')
 const luz = document.createElement('div')
 
-ovni.appendChild(luz)
-
-luz.classList.add('luz-ovni')
-
 setTimeout(() => {
-
-}, 0)//2100
+    ovni.appendChild(luz)
+    luz.classList.add('luz-ovni')
+}, 2100)
