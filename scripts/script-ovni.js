c59a06b5b33d6284826936b98d45057ce9f5b15f
Guilherme Monni <gmonni20@gmail.com>
2025-01-15 18:59:18 -0300

    org script ovni

--- scripts/script-ovni.js
+++ scripts/script-ovni.js
@@ -1,6 +1,14 @@
 // animacao ovni
-const ovni = document.querySelector('.ovni')
-const luz = document.createElement('div')
+function seletor(item){ //seleciona element
+    return item = document.querySelector(item)
+}
+function criar(item){ //cria elemento
+    return item = document.createElement(item)
+}
+
+const ovni = seletor('.ovni') //div ovni
+const sombra = seletor('.sombra')//div sombra
+const luz = criar('div')//div luz
 
 setTimeout(() => {
     ovni.appendChild(luz)
