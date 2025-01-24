148fb3b54be79c5990ccc1f191112c081bd53235
Guilherme Monni <gmonni20@gmail.com>
2025-01-24 19:21:32 -0300

    abducao complete

--- scripts/script-ovni.js
+++ scripts/script-ovni.js
@@ -14,21 +14,32 @@ const sombra = seletor('.sombra')//div sombra
 const et = seletor('.alien') //div alien
 const luz = criar('div')//div luz
 
-setTimeout(() => {
-    ovni.appendChild(luz)//cria luz
-    luz.classList.add('luz-ovni')//classe luz
-    sombra.style.opacity = 1 //sombra alien
-
-    //abducao
+function abducao() {
+    return new Promise((resolve) => {
     setTimeout(() => {
-        et.style.animation = 'abducao 1s 1 linear' //animacao alien
-        sombra.style.animation = 'desSombra 1s 1 linear' //sombra alien
-    }, 800)
+        ovni.appendChild(luz)//cria luz
+        luz.classList.add('luz-ovni')//classe luz
+        sombra.style.opacity = 1 //sombra alien
+    
+        //abducao
+        setTimeout(() => {
+            et.style.animation = 'abducao 1s 1 linear' //animacao alien
+            sombra.style.animation = 'desSombra 1s 1 linear' //sombra alien
+        }, 400)
+    
+        setTimeout(() => {
+            et.style.display = 'none'
+            luz.style.display = 'none'
+            ovni.style.animation = 'desOvni 0.4s 1 linear'
+            setTimeout(() => {
+                resolve()
+            }, 100)
+        }, 1400)
+    }, 1200)
+    })
+} 
 
-    setTimeout(() => {
-        et.style.display = 'none'
-        luz.style.display = 'none'
-        ovni.style.animation = 'desOvni 0.4s 1 linear'
-    }, 1400)
-}, 1800)
-
+abducao().then(() => {
+    sumir(ovni)
+    sumir(et)
+})
